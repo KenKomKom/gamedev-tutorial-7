b1d5b90486c9cddea14999344c7b73923f316e57
KenKomKom <kenichikomala@gmail.com>
2024-04-18 17:16:32 +0700

    minor bug fix

--- scripts/WinScreen.gd
+++ scripts/WinScreen.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,3 +18,4 @@ func _ready():
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://scenes/Level1.tscn")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
